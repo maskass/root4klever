48699ad5c8924e92b964538e8cbcdb635f83c986
Valerio Mascagna <valerio.mascagna@pv.infn.it>
2021-08-16 16:02:41 +0200

    updated ascii2root.cc to comply with the STORM data format

--- ascii2root.cc
+++ ascii2root.cc
@@ -1,23 +1,43 @@
 /*
-Valerio, KLEVER 2018
+Valerio, KLEVER 2021
+
 -------------------
-     <row 1>
-     1        [1]  n event
-     2        [1]  unix time
-     3--6     [4]  tele x,y,x,y
-     7--11    [5]  gonio parameters
-     12--13   [2]  step, info
-     14--37   [24] 1 digi = 8 * (BASELINE, MAX, MAX TIME)
-     38--45   [8]  nclu on beam chambers and barca
-     <row 2>
+     <row 1, EVENT N, 104 fields>
+     0--7     [8]   xRaw
+     8--15    [8]   nStripHit
+     16--23   [8]   nHit
 
-     <row 3>
-     ...
-     ....
-     <row 9>
-     1--10    [10] 5 cluster position + 5 cluster ph
-     11       [1]  total charge on the silicon plane
+     24--31   [8]   digitizer 1, baselines
+     32--39   [8]   digitizer 2, baselines 
+     40--47   [8]   digitizer 3, baselines
 
+     48--49   [8]   digitizer 1, pulse heights
+     56--63   [8]   digitizer 2, pulse heights
+     64--71   [8]   digitizer 3, pulse heights 
+
+     72--79   [8]   digitizer 1, time of max
+     80--87   [8]   digitizer 2, time of max
+     88--95   [8]   digitizer 3, time of max
+
+     96--100  [5]   goniometer parameters
+
+     101      [1]   number of spill
+     102      [1]   number of step
+     103      [1]   event number
+
+    
+    <row 2>
+    0--263    [263] waveform nr 1
+    
+    <row 3>
+    0--263    [263] waveform nr 2
+    
+    <row 4>
+    0--263    [263] waveform nr 3
+
+    <row5, EVENT N+1, 104 fields>
+    ...
+    ...
 */
 
 #include <string>
@@ -27,7 +47,7 @@ Valerio, KLEVER 2018
 #include "TFile.h"
 #include "TTree.h"
 
-void ascii2root(const int ndigi=1, string inFileName="dummy", string outFileName="out") {
+void ascii2root(string inFileName="dummy", string outFileName="out") {
 
     
 //  string outFileName = "root_"+removeExtension(inFileName)+".root";
@@ -40,135 +60,79 @@ void ascii2root(const int ndigi=1, string inFileName="dummy", string outFileName
 
   // OUTPUT TREE DEFINITION  
   TFile* outFile = new TFile(outFileName.c_str(),"RECREATE"); 
-  TTree* t = new TTree("t","Klever tree from ascii file 2018");
+  TTree* t = new TTree("t","Klever tree from ascii file 2021");
   
   // silicon trackers
-  Float_t telePos[4];
-  t->Branch("telePos",telePos,"telePos[4]/F");
-  Int_t bcClu[8];
-  t->Branch("bcClu",bcClu,"bcClu[8]/I");
+  Float_t xRaw[8];        t->Branch("xRaw",xRaw,"xRaw[8]/F");
+  Int_t nStripHit[8];     t->Branch("nStripHit",nStripHit,"nStripHit[8]/I");
+  Int_t nHit[8];          t->Branch("nHit",nHit,"nHit[8]/I");
 
-  Float_t bc4Pos[5],bc4Charge[5],bc4TotCharge;
-  t->Branch("bc4Pos",bc4Pos,"bc4Pos[5]/F");  
-  t->Branch("bc4Charge",bc4Charge,"bc4Charge[5]/F"); 
-  t->Branch("bc4TotCharge",&bc4TotCharge,"bc4TotCharge/I");
-  Float_t bc5Pos[5],bc5Charge[5],bc5TotCharge;
-  t->Branch("bc5Pos",bc5Pos,"bc5Pos[5]/F");  
-  t->Branch("bc5Charge",bc5Charge,"bc5Charge[5]/F"); 
-  t->Branch("bc5TotCharge",&bc5TotCharge,"bc5TotCharge/I");
-  Float_t bc6Pos[5],bc6Charge[5],bc6TotCharge;
-  t->Branch("bc6Pos",bc6Pos,"bc6Pos[5]/F");  
-  t->Branch("bc6Charge",bc6Charge,"bc6Charge[5]/F"); 
-  t->Branch("bc6TotCharge",&bc6TotCharge,"bc6TotCharge/I");
-  Float_t bc7Pos[5],bc7Charge[5],bc7TotCharge;
-  t->Branch("bc7Pos",bc7Pos,"bc7Pos[5]/F");  
-  t->Branch("bc7Charge",bc7Charge,"bc7Charge[5]/F"); 
-  t->Branch("bc7TotCharge",&bc7TotCharge,"bc7TotCharge/I");
-  Float_t bc8Pos[5],bc8Charge[5],bc8TotCharge;
-  t->Branch("bc8Pos",bc8Pos,"bc8Pos[5]/F");  
-  t->Branch("bc8Charge",bc8Charge,"bc8Charge[5]/F"); 
-  t->Branch("bc8TotCharge",&bc8TotCharge,"bc8TotCharge/I");
-  Float_t bc9Pos[5],bc9Charge[5],bc9TotCharge;
-  t->Branch("bc9Pos",bc9Pos,"bc9Pos[5]/F");  
-  t->Branch("bc9Charge",bc9Charge,"bc9Charge[5]/F"); 
-  t->Branch("bc9TotCharge",&bc9TotCharge,"bc9TotCharge/I");
-  Float_t bc10Pos[5],bc10Charge[5],bc10TotCharge;
-  t->Branch("bc10Pos",bc10Pos,"bc10Pos[5]/F");  
-  t->Branch("bc10Charge",bc10Charge,"bc10Charge[5]/F"); 
-  t->Branch("bc10TotCharge",&bc10TotCharge,"bc10TotCharge/I");
-  Float_t bc11Pos[5],bc11Charge[5],bc11TotCharge;
-  t->Branch("bc11Pos",bc11Pos,"bc11Pos[5]/F");  
-  t->Branch("bc11Charge",bc11Charge,"bc11Charge[5]/F"); 
-  t->Branch("bc11TotCharge",&bc11TotCharge,"bc11TotCharge/I");
-  
 
-  // digitizer
-  Int_t digiBase[8*ndigi]; 
-  Int_t digiPh[8*ndigi]; 
-  Int_t digiTime[8*ndigi];
-  t->Branch("digiBase",digiBase,Form("digiBase[%d]/I",8*ndigi));
-  t->Branch("digiPh",digiPh,Form("digiPh[%d]/I",8*ndigi));
-  t->Branch("digiTime",digiTime,Form("digiTime[%d]/I",8*ndigi));
-  // gonio
-  Float_t gonio[5];
-  t->Branch("gonio",gonio,"gonio[5]/F");
-  Int_t spill, step, eventNumber, unixTime;
-  Float_t info;
+  // Digitizer baselines, pulse heights and time of max
+  const int ndigi = 3 ;
+  Int_t digiBase[8*ndigi];    t->Branch("digiBase",digiBase,Form("digiBase[%d]/I",8*ndigi));
+  Int_t digiPh[8*ndigi];      t->Branch("digiPh",digiPh,Form("digiPh[%d]/I",8*ndigi));
+  Int_t digiTime[8*ndigi];    t->Branch("digiTime",digiTime,Form("digiTime[%d]/I",8*ndigi));
+
+  // Goniometer
+  Float_t gonio[5];           t->Branch("gonio",gonio,"gonio[5]/F");
+
+  // Spill, step and event... numbers.
+  Int_t spill, step, eventNumber;
   t->Branch("spill",&spill,"spill/I");
   t->Branch("step",&step,"step/I");
   t->Branch("eventNumber",&eventNumber,"eventNumber/I");
-  t->Branch("unixTime",&unixTime,"unixTime/I");
-  t->Branch("info",&info,"info/F");
 
+  // Waveforms
+
+  Int_t wave0[256], wave1[256], wave2[256];
+  t->Branch("wave0",wave0,"wave0[256]/I");
+  t->Branch("wave1",wave1,"wave1[256]/I");
+  t->Branch("wave2",wave2,"wave2[256]/I");
 
   // end of OUTPUT TREE DEF
-  
+  Int_t dummy=0;
+
   if (myfile.is_open()) {
     
     while(getline(myfile,line)) {
       std::stringstream ss(line);      
-      // first lines
-      
-      if (lineNumber%9==0) {
-	if (lineNumber>0) {
-	  if (abs(telePos[0]+telePos[1]+telePos[2]+telePos[3])<100) {
-	    t->Fill();
-	  }
-	}
-	ss >> unixTime >> eventNumber ;
-	for (Int_t i=0;i<4;i++) ss >> telePos[i];
-	for (Int_t i=0;i<5;i++) ss >> gonio[i];
-	ss >> spill >> step ;
-	for (Int_t idigi=0;idigi<8*ndigi;idigi++) ss >> digiBase[idigi] ;
-	for (Int_t idigi=0;idigi<8*ndigi;idigi++) ss >> digiPh[idigi] ;
-	for (Int_t idigi=0;idigi<8*ndigi;idigi++) ss >> digiTime[idigi] ;
-	for (Int_t i=0;i<8;i++) ss >> bcClu[i];
 
+      // HEADER LINES
+      if (lineNumber%4==0) {
 
+        	if (lineNumber>0) t->Fill();
+
+          for (Int_t i=0;i<8;i++) ss >> xRaw[i];
+          for (Int_t i=0;i<8;i++) ss >> nStripHit[i];
+          for (Int_t i=0;i<8;i++) ss >> nHit[i];
+
+          for (Int_t idigi=0;idigi<8*ndigi;idigi++) ss >> digiBase[idigi] ;
+          for (Int_t idigi=0;idigi<8*ndigi;idigi++) ss >> digiPh[idigi] ;
+          for (Int_t idigi=0;idigi<8*ndigi;idigi++) ss >> digiTime[idigi] ;
+          
+          for (Int_t i=0;i<5;i++) ss >> gonio[i];
+
+          ss >> spill >> step >> eventNumber ;
       }
-      // OPPURE 
-      if (lineNumber%9==1) {
-	for (Int_t i=0;i<5;i++) ss >> bc4Pos[i];
-	for (Int_t i=0;i<5;i++) ss >> bc4Charge[i];
-	ss >> bc4TotCharge;
+
+      // OTHER LINES (Waveforms) 
+      if (lineNumber%4==1) {
+        for (Int_t i=0;i<6;i++) ss >> dummy ;
+  	    for (Int_t i=0;i<256;i++) ss >> wave0[i];
+        ss >> dummy ;
       }
-      if (lineNumber%9==2) {
-	for (Int_t i=0;i<5;i++) ss >> bc5Pos[i];
-	for (Int_t i=0;i<5;i++) ss >> bc5Charge[i];
-	ss >> bc5TotCharge;
+      if (lineNumber%4==2) {
+        for (Int_t i=0;i<6;i++) ss >> dummy ;
+        for (Int_t i=0;i<256;i++) ss >> wave1[i];
+        ss >> dummy ;
       }
-      if (lineNumber%9==3) {
-	for (Int_t i=0;i<5;i++) ss >> bc6Pos[i];
-	for (Int_t i=0;i<5;i++) ss >> bc6Charge[i];
-	ss >> bc6TotCharge;
+      if (lineNumber%4==3) {
+        for (Int_t i=0;i<6;i++) ss >> dummy ;
+        for (Int_t i=0;i<256;i++) ss >> wave2[i];
+        ss >> dummy ;
       }
-      if (lineNumber%9==4) {
-	for (Int_t i=0;i<5;i++) ss >> bc7Pos[i];
-	for (Int_t i=0;i<5;i++) ss >> bc7Charge[i];
-	ss >> bc7TotCharge;
-      }
-      if (lineNumber%9==5) {
-	for (Int_t i=0;i<5;i++) ss >> bc8Pos[i];
-	for (Int_t i=0;i<5;i++) ss >> bc8Charge[i];
-	ss >> bc8TotCharge;
-      }
-      if (lineNumber%9==6) {
-	for (Int_t i=0;i<5;i++) ss >> bc9Pos[i];
-	for (Int_t i=0;i<5;i++) ss >> bc9Charge[i];
-	ss >> bc9TotCharge;
-      }
-      if (lineNumber%9==7) {
-	for (Int_t i=0;i<5;i++) ss >> bc10Pos[i];
-	for (Int_t i=0;i<5;i++) ss >> bc10Charge[i];
-	ss >> bc10TotCharge;
-      }
-      if (lineNumber%9==8) {
-	for (Int_t i=0;i<5;i++) ss >> bc11Pos[i];
-	for (Int_t i=0;i<5;i++) ss >> bc11Charge[i];
-	ss >> bc11TotCharge;
-      }
-      
-    
+
       lineNumber++;
     }
       
