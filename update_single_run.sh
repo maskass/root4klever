b7783d5e1f95dc04e8a96290938e9c75696603bf
Valerio Mascagna <valerio.mascagna@pv.infn.it>
2021-08-19 16:33:10 +0200

    update new naming of the ascii files

--- update_single_run.sh
+++ update_single_run.sh
@@ -15,7 +15,7 @@ root_merged_dir="/eos/user/m/mmoulson/KLEVER/TestBeamData/Root2021/merged"
 
 new_file_count=0;
 
-for file in $(ls $ascii_dir/run$nrun*.dat); do
+for file in $(ls $ascii_dir"/run"$nrun"_wave_*.dat"); do
     nspill=$(echo $file | awk '{c=split($1,a,"/");split(a[c],b,".");dd=split(b[1],d,"_");print d[dd]+0}')
 
     root_file=$(awk -v run=$nrun -v spill=$nspill 'BEGIN{printf("run%6.6d_%6.6d.root",run,spill)}')
